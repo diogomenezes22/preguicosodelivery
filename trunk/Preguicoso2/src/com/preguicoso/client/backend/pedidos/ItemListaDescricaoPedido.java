477cd13c31cc979c67fa9244eb4537d7e2adde7e
abraaobarros3@gmail.com <abraaobarros3@gmail.com@d2d36b68-3891-acf7-8537-5cc4ba3813b3>
2012-01-17 18:27:33 +0000

    Descricao de pedido

--- trunk/Preguicoso2/src/com/preguicoso/client/backend/pedidos/ItemListaDescricaoPedido.java
+++ trunk/Preguicoso2/src/com/preguicoso/client/backend/pedidos/ItemListaDescricaoPedido.java
@@ -2,20 +2,38 @@ package com.preguicoso.client.backend.pedidos;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.Widget;
+import com.preguicoso.shared.entities.ItemCardapioBean;
 
 public class ItemListaDescricaoPedido extends Composite  {
 
 	private static itemListaDescricaoPedidoUiBinder uiBinder = GWT
 			.create(itemListaDescricaoPedidoUiBinder.class);
+	@UiField InlineLabel no;
+	@UiField InlineLabel observacao;
+	@UiField InlineLabel item;
+	@UiField InlineLabel qtd;
+	@UiField HTMLPanel campo;
 
 	interface itemListaDescricaoPedidoUiBinder extends
 			UiBinder<Widget, ItemListaDescricaoPedido> {
 	}
-
+	public void setOdd(){
+		campo.addStyleName("odd");
+	}
 	public ItemListaDescricaoPedido() {
 		initWidget(uiBinder.createAndBindUi(this));
+	}
+	public ItemListaDescricaoPedido(ItemCardapioBean item) {
+		initWidget(uiBinder.createAndBindUi(this));
+		no.setText(item.getNumero()+"");
+		observacao.setText(item.getObservacao());
+		this.item.setText((item.getNome()));
+		this.qtd.setText(item.getQuantidade()+"");
 	}
 
 
