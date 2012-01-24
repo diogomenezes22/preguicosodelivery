13f9d15d0ed1b7b1e5d487378d3926a621750bca
osmanpontes <osmanpontes@gmail.com>
2012-01-24 18:59:54 +0000

    Add carrinho começa com 1

--- Preguicoso2/src/com/preguicoso/client/Preguicoso.java
+++ Preguicoso2/src/com/preguicoso/client/Preguicoso.java
@@ -3,7 +3,6 @@ package com.preguicoso.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.preguicoso.client.busca.listaCategoria;
 import com.preguicoso.client.login.LoginService;
@@ -21,17 +20,17 @@ public class Preguicoso extends RouterHistory implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		// this.isLogado();
 		LoginUI login = new LoginUI();
 		RootPanel.get("login").clear();
 		RootPanel.get("userDropdownMenu").clear();
 		RootPanel.get("login").add(login);
 
-		Hyperlink link0 = new Hyperlink("index", "index");
-		Hyperlink link1 = new Hyperlink("Cadastrar Restaurante",
-				"cadastroEstabelecimento");
-		Hyperlink link2 = new Hyperlink("Cadastrar Usuario", "cadastroUsuario");
-		Hyperlink link3 = new Hyperlink("Cardapio", "cardapio/3");
+		// Hyperlink link0 = new Hyperlink("index", "index");
+		// Hyperlink link1 = new Hyperlink("Cadastrar Restaurante",
+		// "cadastroEstabelecimento");
+		// Hyperlink link2 = new Hyperlink("Cadastrar Usuario",
+		// "cadastroUsuario");
+		// Hyperlink link3 = new Hyperlink("Cardapio", "cardapio/3");
 
 		RootPanel.get("cadastroEstabelecimento").add(new listaCategoria());
 
